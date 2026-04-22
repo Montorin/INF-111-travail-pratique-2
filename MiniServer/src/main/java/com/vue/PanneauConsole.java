e98121ac014a6cec058da40764801e8467c44b5c
Momo <jalloulmohamadnajib@gmail.com>
2026-04-22 16:25:14 -0400

    Question 4.1

--- MiniServer/src/main/java/com/vue/PanneauConsole.java
+++ MiniServer/src/main/java/com/vue/PanneauConsole.java
@@ -1,21 +1,22 @@
 package com.vue;
 
 import com.gestionnaireLivraisons.GestionnaireLivraisons;
+import com.observer.Observable;
+import com.observer.Observateur;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 /**
  * La classe qui constitue la console dans l'interface graphique.
  *
  */
-public class PanneauConsole extends JPanel
+public class PanneauConsole extends JPanel implements Observateur
 {
 
     private JTextArea console;
     private GestionnaireLivraisons gestionnaireLivraisons;
-
-    // TODO : À compléter/modifier
 
     /**
      * Constructeur pour cette classe.
@@ -23,9 +24,27 @@ public class PanneauConsole extends JPanel
      * @param gestionnaireLivraisons Le gestionnaire de livraisons associé.
      */
     public PanneauConsole(GestionnaireLivraisons gestionnaireLivraisons) {
-        // TODO : À compléter/modifier
-        System.err.println("Méthode PanneauConsole.PanneauConsole non implémentée.");
+        this.gestionnaireLivraisons = gestionnaireLivraisons;
+        this.gestionnaireLivraisons.ajouterObservateur(this);
+
+        this.setLayout(new BorderLayout());
+
+        this.console = new JTextArea(10, 40);
+        this.console.setEditable(false);
+        this.console.setBackground(Color.BLACK);
+        this.console.setForeground(Color.GREEN);
+        this.console.setFont(Config.fonte);
+
+        JScrollPane scrollPane = new JScrollPane(this.console);
+        this.add(scrollPane, BorderLayout.CENTER);
     }
 
-    // TODO : À compléter/modifier
+    public void seMettreAJour(Observable observable) {
+        String trace = this.gestionnaireLivraisons.consommerTrace();
+        if (trace != null && !trace.isEmpty()) {
+            this.console.append(trace + "\n");
+            this.console.setCaretPosition(this.console.getDocument().getLength());
+        }
+    }
+
 }
