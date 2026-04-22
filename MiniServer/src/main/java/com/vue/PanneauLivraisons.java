cf36d90daa8d649d672ead8dfb3ab960ab46cc20
Momo <jalloulmohamadnajib@gmail.com>
2026-04-22 16:16:22 -0400

    Question 3.1

--- MiniServer/src/main/java/com/vue/PanneauLivraisons.java
+++ MiniServer/src/main/java/com/vue/PanneauLivraisons.java
@@ -5,6 +5,7 @@ import com.gestionnaireLivraisons.*;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
+import java.util.Vector;
 
 /**
  * La classe qui constitue la table des livraisons.
@@ -25,10 +26,54 @@ public class PanneauLivraisons extends JPanel {
      * @param gestionnaireLivraisons Le gestionnaire de livraisons associé.
      */
     public PanneauLivraisons(GestionnaireLivraisons gestionnaireLivraisons) {
-        // TODO : À compléter/modifier
-        System.err.println("Méthode PanneauLivraisons.PanneauLivraisons non implémentée.");
+        this.gestionnaireLivraisons = gestionnaireLivraisons;
+
+        this.tableLivraisons = new ComposantTable("liste des livraisons", 450, 250, this.nomsColonnes, this.donneesCentrees);
+
+        this.setLayout(new BorderLayout());
+        this.add(tableLivraisons, BorderLayout.CENTER);
+
+        this.mettreAJour();
     }
 
+    public void mettreAJour() {
+        this.tableLivraisons.mettreAJour(this.calculerDonnees());
+    }
 
-    // TODO : À compléter/modifier
+    private Vector<Vector<String>> calculerDonnees() {
+        Vector<Vector<String>> donnees = new Vector<>();
+
+        for(Livraison livraison : this.gestionnaireLivraisons.getLivraisonsAEffectuer()){
+            donnees.add(this.creerLigne(livraison));
+        }
+
+        for(Livreur livreur : this.gestionnaireLivraisons.getLivreursEnregistres()){
+            for (Livraison livraison : livreur.getLivraisonsEnCours()){
+                donnees.add(this.creerLigne(livraison));
+            }
+            for (Livraison livraison : livreur.getLivraisonsEffectuees()){
+                donnees.add(this.creerLigne(livraison));
+            }
+        }
+
+        for(Livraison livraison : this.gestionnaireLivraisons.getLivraisonsAEffectuer()){
+            donnees.add(this.creerLigne(livraison));
+        }
+        return donnees;
+    }
+
+    private Vector<String> creerLigne(Livraison livraison) {
+        Vector<String> ligne = new Vector<>();
+        ligne.add(String.valueOf(livraison.getId()));
+        ligne.add(String.valueOf(livraison.getLot()));
+        ligne.add(livraison.getPriorite().toString());
+        ligne.add(String.valueOf(livraison.getTentative()));
+        ligne.add(this.formaterStatut(livraison.getStatut()));
+        return ligne;
+    }
+
+    private String formaterStatut(Statut statut) {
+        return statut.name().replace("_", " ");
+    }
+
 }
